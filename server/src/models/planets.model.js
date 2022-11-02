45c578fbaea53d9917ee4964ffdbb1b01750cf0e
alienao <armin.orujyan@gmail.com>
2022-11-02 21:51:10 +0400

    feat: Loading Data On Startup

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,6 @@
-const { parse } = require('csv-parse');
 const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
 
 const habitablePlanets = [];
 
@@ -9,7 +10,19 @@ function isHabitablePlanet(planet) {
     && planet['koi_prad'] < 1.6;
 }
 
-fs.createReadStream('kepler_data.csv')
+/*
+const promise = new Promise((resolve, reject) =>{
+  resolve(42);
+});
+promise.then((result) => {
+
+});
+const result = await promise;
+console.log(result);
+ */
+function loadPlanetsData(){
+  return new Promise((resolve, reject) => {
+  fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
   .pipe(parse({
     comment: '#',
     columns: true,
@@ -21,14 +34,16 @@ fs.createReadStream('kepler_data.csv')
   })
   .on('error', (err) => {
     console.log(err);
+    reject(err);
   })
   .on('end', () => {
-    console.log(habitablePlanets.map((planet) => {
-      return planet['kepler_name'];
-    }));
     console.log(`${habitablePlanets.length} habitable planets found!`);
+    resolve();
   });
+} );
+} 
 
   module.exports = {
+    loadPlanetsData,
     planets: habitablePlanets,
-  };
+  }; 
